02926fbba63ce71b5bf7f573acf2f0aaa28ccae6
TUSHAR KASAUDHAN <tusharkasaudhan6@gmail.com>
2022-06-26 22:39:17 +0530

    Update all.sh

--- all.sh
+++ all.sh
@@ -12,18 +12,18 @@ sudo sed -i --follow-symlinks 's/^SELINUX=enforcing/SELINUX=disabled/' /etc/sysc
 sudo sh -c "cp /etc/sysconfig/iptables /etc/sysconfig/iptables.ORIG && iptables --flush && iptables --flush && iptables-save > /etc/sysconfig/iptables"
 sudo systemctl restart iptables.service
 
-# Load/Enable br_netfilter kernel module and make persistent
-sudo modprobe br_netfilter
-sudo sh -c "echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables"
-sudo sh -c "echo '1' > /proc/sys/net/bridge/bridge-nf-call-ip6tables"
-sudo sh -c "echo 'net.bridge.bridge-nf-call-iptables=1' >> /etc/sysctl.conf"
-sudo sh -c "echo 'net.bridge.bridge-nf-call-ip6tables=1' >> /etc/sysctl.conf"
+sudo sh -c 'cat >>/etc/sysctl.d/kubernetes.conf<<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF'
 
-# Install dependencies for docker-ce
-sudo yum -y install yum-utils device-mapper-persistent-data lvm2
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
 
-# Add the docker-ce repository
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+sudo yum install -y docker-ce-19.03.12 
+
+sudo systemctl enable --now docker
 
 # Add the Kubernetes Repository
 sudo sh -c 'cat <<EOF > /etc/yum.repos.d/kubernetes.repo
@@ -37,11 +37,6 @@ gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
        https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOF'
 
-# Update yum cache after adding repository
-sudo yum -y makecache fast
-
-# Install latest supported docker runtime (18.06 is the latest runtime supported by Kubernetes v1.13.2)
-sudo yum -y install docker-ce-18.06.1.ce
 
 # Install Kubernetes
 sudo yum -y install kubelet kubeadm kubectl
@@ -61,5 +56,3 @@ sudo reboot
 # Check what cgroup driver that docker is using
 sudo docker info | grep -i cgroup
 
-# Add the cgroup driver from the previous step to the kublet config as an extra argument
-sudo sed -i "s/^\(KUBELET_EXTRA_ARGS=\)\(.*\)$/\1\"--cgroup-driver=$(sudo docker info | grep -i cgroup | cut -d" " -f3)\2\"/" /etc/sysconfig/kubelet
