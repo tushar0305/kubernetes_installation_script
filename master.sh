886c5996d441e6a5c4e93369d2e3afda7c6bb0cf
TUSHAR KASAUDHAN <tusharkasaudhan6@gmail.com>
2022-06-27 01:28:19 +0530

    Update master.sh

--- master.sh
+++ master.sh
@@ -1,5 +1,5 @@
-sudo kubeadm init --ignore-preflight-errors=all
-sudo kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f https://docs.projectcalico.org/v3.14/manifests/calico.yaml
+kubeadm init --ignore-preflight-errors=all
+kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f https://docs.projectcalico.org/v3.14/manifests/calico.yaml
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
